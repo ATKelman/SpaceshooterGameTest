b0d4ec0a68058fc7850511e4a2473d47329c32d6
ATKelman <a14aleke@student.his.se>
2017-04-05 17:41:09 +0200

    Ship / Driver movement
    Added Driver (glömde den hette ship) controller
    Fixed some github specifics for unity

---
+++ SpaceShooterTest/Assets/Characters/Driver/DriverController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DriverController : MonoBehaviour
+{
+    public float forwardSpeed;
+    public float sideSpeed;
+    public float rotateSpeed;
+    public float forwardMultiplier = 2f;
+
+    private bool moveLeft;
+    private bool moveRight;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+            moveRight = true;
+        else if (Input.GetKeyUp(KeyCode.D))
+            moveRight = false;
+
+        if (Input.GetKeyDown(KeyCode.A))
+            moveLeft = true;
+        else if (Input.GetKeyUp(KeyCode.A))
+            moveLeft = false;
+
+        if (moveLeft && !moveRight)
+        {
+            GetComponent<Rigidbody2D>().AddForce(-transform.right * sideSpeed);
+            GetComponent<Rigidbody2D>().AddForce(transform.up * forwardSpeed);
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+        }
+
+        if (moveRight && !moveLeft)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.right * sideSpeed);
+            GetComponent<Rigidbody2D>().AddForce(transform.up * forwardSpeed);
+            transform.Rotate(Vector3.forward * -Time.deltaTime * rotateSpeed);
+        }
+
+        if (moveLeft && moveRight)
+            GetComponent<Rigidbody2D>().AddForce(transform.up * forwardSpeed * forwardMultiplier);
+
+
+
+    }
+}
