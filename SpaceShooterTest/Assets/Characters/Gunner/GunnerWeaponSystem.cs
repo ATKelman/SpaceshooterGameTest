43926cd6cf0bb9eca788c984bf67972d6bb3f5cf
ATKelman <a14aleke@student.his.se>
2017-04-08 19:31:41 +0200

    Work on Gunner Laser
    Added raycast to laser to detect hits
    Work in progress - second line renderer around laser

--- SpaceShooterTest/Assets/Characters/Gunner/GunnerWeaponSystem.cs
+++ SpaceShooterTest/Assets/Characters/Gunner/GunnerWeaponSystem.cs
@@ -3,67 +3,38 @@ using System.Collections;
 
 public class GunnerWeaponSystem : MonoBehaviour
 {
-    public Transform start;
+    public GameObject laserPrefab;
+    public GameObject ship;
 
     public float laserDistance = 10f;
     public float laserTime = 1.0f;
     public float laserWidth = 1.0f;
 
-    private LineRenderer laser;
-
     private float timeSinceFire = 0f;
     private bool firing;
-
-    public Vector3 startPos;
-    public Vector3 targetPos;
-
-    void Start()
-    {
-        laser = GetComponent<LineRenderer>();
-        laser.SetWidth(laserWidth, laserWidth);
-    }
-
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !firing)
-            Shoot();
+            ShootLaser();
 
-        //if(firing)
-        //{
-        //    timeSinceFire += Time.deltaTime;
-        //    if(timeSinceFire >= laserTime)
-        //    {
-        //        firing = false;
-        //        laser.SetPosition(1, transform.position);
-        //    }
-        //}
+        if (firing)
+        {
+            timeSinceFire += Time.deltaTime;
+            if (timeSinceFire >= laserTime)
+            {
+                firing = false;
+                //laser.SetPosition(1, transform.position);
+                laserPrefab.SetActive(false);
+            }
+        }
     }
 
-    void Shoot()
+    void ShootLaser()
     {
         firing = true;
         timeSinceFire = 0;
-        laser.SetPosition(0, transform.position);
-        laser.SetPosition(1, transform.up * laserDistance);
-        targetPos = transform.up * laserDistance;
-        AddCollider();
-    }
-
-    void AddCollider()
-    {
-        BoxCollider col = new GameObject("Collider").AddComponent<BoxCollider>();
-        col.transform.parent = laser.transform;
-        col.size = new Vector3(laserDistance, laserWidth, 1f);
-        Vector3 midPoint = (start.position + targetPos) / 2;
-        col.transform.position = midPoint;
-
-        float angle = (Mathf.Abs(startPos.y - targetPos.y) / Mathf.Abs(startPos.x - targetPos.x));
-        if((startPos.y < targetPos.y && startPos.x > targetPos.x) || (targetPos.y < startPos.y && targetPos.x > startPos.x))
-        {
-            angle *= -1;
-        }
-        angle = Mathf.Rad2Deg * Mathf.Atan(angle);
-        col.transform.Rotate(0, 0, angle);
+        laserPrefab.GetComponent<Weapon_Laser>().CreateLaser(laserDistance, laserWidth, ship.transform.position, (ship.transform.up * laserDistance));
+        laserPrefab.SetActive(true);
     }
 }
---
+++ SpaceShooterTest/Assets/Characters/Gunner/Weapon_Laser.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon_Laser : MonoBehaviour
+{
+    public LayerMask layer;
+    public GameObject secondRenderer;
+
+    private float laserDistance;
+    private float laserWidth;
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private Vector3 targetPos;
+
+    private LineRenderer laser;
+
+    private void Awake()
+    {
+        laser   = GetComponent<LineRenderer>();
+    }
+
+    public void CreateLaser(float distance, float width, Vector3 start, Vector3 end)       
+    {
+        laserDistance   = distance;
+        laserWidth      = width;       
+        startPos        = start;
+        endPos          = end;
+        AddLineRenderer();
+        AddSecondLineRenderer();
+        RaycastLaser();
+    }
+
+    // Create a line between point of fire and target point
+    void AddLineRenderer()
+    {
+        laser.SetWidth(laserWidth, laserWidth);
+        laser.SetPosition(0, startPos);
+        targetPos = startPos + endPos;
+        laser.SetPosition(1, targetPos);
+    }
+
+    void AddSecondLineRenderer()
+    {
+        LineRenderer laser2 = secondRenderer.GetComponent<LineRenderer>();
+        laser2.transform.position = laser.transform.position;
+        laser2.SetWidth(0.1f, 0.1f);
+        laser2.SetColors(Color.blue, Color.blue);
+
+        float xInc = targetPos.x - startPos.x;
+        xInc = xInc / 20;
+
+        float yMin, yMax;
+        if(startPos.y > targetPos.y)
+        {
+            yMin = targetPos.y;
+            yMax = startPos.y;
+        }
+        else
+        {
+            yMin = startPos.y;
+            yMax = targetPos.y;
+        }
+        for (int i = 0; i < 20; i++)
+        {
+
+            Vector3 pos = new Vector3(i * xInc, Mathf.Sin(i + Time.time * Random.Range(yMin, yMax)), 0f);
+            //Vector3 direction = targetPos - startPos;
+            //pos = pos * direction;
+            pos = pos + startPos;
+            laser2.SetPosition(i, pos);
+        }
+    }
+
+    // Raycast to see if enemy is hit
+    void RaycastLaser()
+    {
+        Vector3 direction = targetPos - startPos;
+        // Ensure that the layer mask is enemy layer
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(startPos.x, startPos.y),
+           new Vector2(direction.x, direction.y), laserDistance, layer);
+        if(hit.collider != null)
+        {     
+            print("hit");
+        }
+        else
+        {
+            print("miss");
+        }
+    }
+}
